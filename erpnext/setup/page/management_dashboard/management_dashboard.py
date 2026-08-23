e9f3c951a3855349d4904b377e11dc4f847a87ac
Harshit Garg <harshitgarg0001@gmail.com>
2026-08-23 20:41:19 +0530

    update dashboard access

--- erpnext/setup/page/management_dashboard/management_dashboard.py
+++ erpnext/setup/page/management_dashboard/management_dashboard.py
@@ -43,14 +43,8 @@ def _hours_between(in_time, out_time, now=None):
 
 
 def _has_dashboard_access():
-	try:
-		page = frappe.get_cached_doc("Page", "management-dashboard")
-		if not page.roles:
-			return True
-		page_roles = {r.role for r in page.roles}
-		return bool(set(frappe.get_roles()).intersection(page_roles))
-	except Exception:
-		return False
+	page_roles = frappe.get_all("Page Role", filters={"parent": "management-dashboard"}, pluck="role")
+	return bool(set(frappe.get_roles()).intersection(set(page_roles)))
 
 
 @frappe.whitelist()
