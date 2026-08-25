3a420540b4b451829463505c5ea64c9f1452eb5c
Harshit Garg <harshitgarg0001@gmail.com>
2026-08-25 10:12:23 +0530

    added welcome back on dashboard

--- erpnext/setup/page/management_dashboard/management_dashboard.js
+++ erpnext/setup/page/management_dashboard/management_dashboard.js
@@ -132,7 +132,7 @@ erpnext.ManagementDashboard = class ManagementDashboard {
 					<div class="dashboard-subtitle">
 						<span>Company Overview</span>|<span>Today</span>|<span>Current Month</span>
 					</div>
-					<div class="dashboard-welcome">Welcome back, ${frappe.boot.user.full_name || frappe.session.user}</div>
+					<div class="dashboard-welcome">Welcome back, ${frappe.boot.user.full_name || frappe.session.user} here's your team's live status.</div>
 				</div>
 				<div class="kpi-row"></div>
 				<div class="section-card attendance-section">
